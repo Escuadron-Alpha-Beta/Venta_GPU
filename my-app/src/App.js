1ad1121f3e07a82ff5e0d6c8045491c3107ed0f1
Ricardo Sanchez <rasanchez@unitec.edu>
2021-05-24 18:30:39 -0600

    Visual y Blockchain v1.0

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'reactstrap'; 
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -14,16 +21,28 @@ class App extends Component {
   render() {
     console.log(this.props);
     return (
-      <div >
-
+      <div>
+        <Navbar bg="dark" variant="dark" fixed="top">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/Productos">Productos</Nav.Link>
+            <Nav.Link href="/Perfil">Perfil</Nav.Link>
+            <Nav.Link href="/Login">Log In</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+        </Navbar>
+        <br />
+        <br/>
+        <br/>
         <div id="Header">
-
           <BrowserRouter>
             <div>
               aqui poner lo del header
-              <div>
-                
-              </div>
+              
               <Switch>
                 <Route path="/" component={Home} exact/>
                 <Route path="/Home" component={Home}exact />
--- my-app/src/Paginas/Navbar.js
+++ my-app/src/Paginas/Navbar.js
@@ -1,24 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'reactstrap'; 
 
-class Navbar extends Component{
-    render() {
-        return (
-          <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-            
-            <a
-              className="navbar-brand col-sm-3 col-md-2 mr-0"
-              href=""
-            >
-              <img src={Logo} height='40px' style={{marginRight: 10 + 'px'}}></img>
-                 Graphics Marketplace
-            </a>
-            <ul className="navbar-nav px-3">
-              <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-                <small className="text-white"><span id="account">{this.props.account}</span></small>
-              </li>
-            </ul>
-          </nav>
-        );
-    }
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark" fixed="top">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/Productos">Productos</Nav.Link>
+            <Nav.Link href="/Perfil">Perfil</Nav.Link>
+            <Nav.Link href="/Login">Log In</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
+        <br/>
+        <br/>
+        <br/>
+      </div>
+    );
+  }
 }
 export default Navbar;
