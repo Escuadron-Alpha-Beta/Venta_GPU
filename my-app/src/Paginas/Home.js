32b50e8f98bd239adbb3bbec0fddac2bbd83f64b
Ricardo Sanchez <rasanchez@unitec.edu>
2021-05-25 16:20:45 -0600

    Creacion de todas las paginas

--- my-app/src/Paginas/Home.js
+++ my-app/src/Paginas/Home.js
@@ -1,34 +1,44 @@
 import React from 'react';
 import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer, MDBCard, MDBCardTitle, MDBBtn, MDBRow, MDBCol, MDBIcon } from "mdbreact";
 import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 //var firebase = require("firebase");
 
-class Home extends React.Component{
-    constructor(props){
+class Home extends React.Component {
+    constructor(props) {
         super(props)
-        this.state = {img:""}
+        this.state = { img: "" }
     }
-    componentDidMount(){
-        
-    }
-    render(){
-        return(
-            <Container>
-                <MDBContainer>
-                    <h4>Una Compañia Hecha Para Gamers</h4>
-                    <MDBCarousel activeItem={1} length={3} showControls={true} showIndicators={true} className="z-depth-1">
+    componentDidMount() {
 
-                    </MDBCarousel>
-                </MDBContainer>
-                <MDBRow>
-                    <MDBCol>
-                        <MDBCard>
-                            <h6>Aqui vienen los items</h6>
-                        </MDBCard>
-                    </MDBCol>
-                </MDBRow>
-            </Container>
+    }
+    render() {
+        return (
+            <div>
+                <Container>
+                    <MDBContainer>
+                        <h4>Una Compañia Hecha Para Gamers</h4>
+                        <MDBCarousel activeItem={1} length={3} showControls={true} showIndicators={true} className="z-depth-1">
+
+                        </MDBCarousel>
+                    </MDBContainer>
+                    <MDBRow>
+                        <MDBCol>
+                            <MDBCard>
+                                <h6>Aqui vienen los items</h6>
+                            </MDBCard>
+                        </MDBCol>
+                    </MDBRow>
+                </Container>
+                <div class="card w-75">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Button</a>
+                    </div>
+                </div>
+            </div>
         )
 
     }
---
+++ my-app/src/Paginas/Productos.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer, MDBCard, MDBCardTitle, MDBBtn, MDBRow, MDBCol, MDBIcon } from "mdbreact";
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class Productos extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = { img: "" }
+    }
+    componentDidMount() {}
+    render() {
+        
+    }
+}
